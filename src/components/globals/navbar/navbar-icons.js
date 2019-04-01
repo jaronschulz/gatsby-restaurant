3ad28cea54cfe01a091c44155c5b6accedd75017
Jaron Schulz <jaronchulz@gmail.com>
2019-04-01 15:58:11 +0200

    added and styled the social links with responsivness

--- src/components/globals/navbar/navbar-icons.js
+++ src/components/globals/navbar/navbar-icons.js
@@ -1,7 +1,69 @@
 import React, { Component } from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+import { styles } from "../../../utils"
+import { FaInstagram, FaTwitter, FaF, FaFacebook } from "react-icons/fa"
 
-export default class componentName extends Component {
+export default class NavbarIcons extends Component {
+  state = {
+    icons: [
+      {
+        id: 1,
+        icon: <FaFacebook className="icon facebook-icon" />,
+        path: "https://www.facebook.com",
+      },
+      {
+        id: 2,
+        icon: <FaTwitter className="icon twitter-icon" />,
+        path: "https://www.twitter.com",
+      },
+      {
+        id: 3,
+        icon: <FaInstagram className="icon instagram-icon" />,
+        path: "https://www.instagram.com",
+      },
+    ],
+  }
   render() {
-    return <div>content from navbar-icons</div>
+    return (
+      <IconWrapper>
+        {this.state.icons.map(icon => (
+          <a
+            href={icon.path}
+            key={icon.id}
+            target="_blank"
+            rel="noopener norefferer"
+          >
+            {icon.icon}
+          </a>
+        ))}
+      </IconWrapper>
+    )
   }
 }
+
+const IconWrapper = styled.div`
+  .icon {
+    font-size: 1.3rem;
+    cursor: pointer;
+    ${styles.transObject({ time: "0.3s" })}
+  }
+  .facebook-icon {
+    color: #3b579d;
+  }
+  .twitter-icon {
+    color: #3ab7f0;
+  }
+  .instagram-icon {
+    color: #da5f53;
+  }
+  .icon:hover {
+    color: ${styles.colors.mainYellow};
+  }
+  display: none;
+  @media (min-width: 768px) {
+    display: flex;
+    width: 10rem;
+    justify-content: space-around;
+  }
+`
