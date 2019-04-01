8495b52a1afe09d61ac9466c9c0bd48ad5b5098e
Jaron Schulz <jaronchulz@gmail.com>
2019-04-01 16:22:53 +0200

    created missing pages and copied from index.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,15 +1,14 @@
 import React from "react"
 import styled from "styled-components"
-import { FaBeer } from "react-icons/fa"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const MenuPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>This is Home</h3>
+    <h3>this is Menu Page</h3>
   </Layout>
 )
 
-export default IndexPage
+export default MenuPage
