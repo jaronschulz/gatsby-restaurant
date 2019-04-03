531c069237e05748c73a625d1993910dc1fdb818
Jaron Schulz <jaronchulz@gmail.com>
2019-04-03 11:59:04 +0200

    Added a Button for the banner using a styled component for most and inline styles for positioning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { HomeHeader, Banner } from "../utils"
+import { HomeHeader, Banner, BannerButton } from "../utils"
 import img from "../images/bcg/homeBcg.jpeg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <HomeHeader img={img}>
-      <Banner title="eatery" subtitle="55 main street - Santa Monica, CA" />
+      <Banner title="eatery" subtitle="55 main street - Santa Monica, CA">
+        <BannerButton style={{ margin: "2rem auto" }}>menu</BannerButton>
+      </Banner>
     </HomeHeader>
   </Layout>
 )
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import * as styles from "./styles"
 import { HomeHeader, PageHeader } from "./header"
 import Banner from "./banner"
+import { BannerButton } from "./button"
 
-export { styles, HomeHeader, PageHeader, Banner }
+export { styles, HomeHeader, PageHeader, Banner, BannerButton }
