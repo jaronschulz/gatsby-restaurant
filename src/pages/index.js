1aec7ae86fb67a48fc8b8a1389cce7c1e89c05dc
Jaron Schulz <jaronchulz@gmail.com>
2019-04-04 17:14:17 +0200

    included gallery with graphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { HomeHeader, Banner, BannerButton } from "../utils"
 import img from "../images/bcg/homeBcg.jpeg"
 import QuickInfo from "../components/HomePageComponents/QuickInfo"
+import Gallery from "../components/HomePageComponents/Gallery"
 
 const IndexPage = () => (
   <Layout>
@@ -13,7 +15,8 @@ const IndexPage = () => (
         <BannerButton style={{ margin: "2rem auto" }}>menu</BannerButton>
       </Banner>
     </HomeHeader>
-    <QuickInfo  />
+    <QuickInfo />
+    <Gallery />
   </Layout>
 )
 
