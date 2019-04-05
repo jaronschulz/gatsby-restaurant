6c91bffa23e29b3722f4e6bfff10b254acfb7519
Jaron Schulz <jaronchulz@gmail.com>
2019-04-05 12:22:30 +0200

    changed query to get all images and then map through to bring on screen - also linked G-1 into index instead

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SEO from "../components/seo"
 import { HomeHeader, Banner, BannerButton } from "../utils"
 import img from "../images/bcg/homeBcg.jpeg"
 import QuickInfo from "../components/HomePageComponents/QuickInfo"
-import Gallery from "../components/HomePageComponents/Gallery"
+import Gallery from "../components/HomePageComponents/Gallery-1"
 
 const IndexPage = () => (
   <Layout>
